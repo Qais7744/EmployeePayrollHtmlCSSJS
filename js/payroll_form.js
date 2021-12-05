982a7ac3715b073220fcc24a0a34e01309329843
unknown <altamashbagwan1@gmail.com>
2021-12-05 22:51:32 +0530

    Added: Functioning of to save th Employee payroll object to local Storage

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -32,6 +32,18 @@ const save = () => {
   }
 }
 
+function createAndUpdateStorage(employeePayrollData) {
+  let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+
+  if (employeePayrollList != undefined) {
+    employeePayrollList.push(employeePayrollData);
+  } else {
+    employeePayrollList = [employeePayrollData];
+  }
+  alert(employeePayrollList.toString());
+  localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
+}
+
 const createEmployeePayroll = () => {
   let employeePayrollData = new EmployeePayrollData();
   try {
